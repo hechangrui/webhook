36af365af6286b71212f844f054df889e5f421ca
root <root@localhost.localdomain>
2022-08-11 11:03:54 +0800

    edit index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 header('Content-type: text/html; charset=utf-8');
 ini_set("error_reporting", "E_ALL & ~E_NOTICE");
 
-echo "Hi，Webhooks！By herry 07<br/>";
+echo "Hi，Webhooks！By herry 08<br/>";
 
 echo '<br/>测试：输出项目路径和用户目录：<br/>';
 
